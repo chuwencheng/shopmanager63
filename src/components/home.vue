9d20a4dfac6d60ab3f107ca6baa2f42adb85c99b
cwc <2534385076@qq.com>
2019-02-15 18:32:04 +0800

    侧边栏完成

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,29 +1,109 @@
 <template>
     <el-container class="container">
+        <!-- 头部 -->
         <el-header class="header">
             <el-row>
                 <el-col :span="4">
                     <div class="grid-content bg-purple">
-                    <img src="../assets/logo.png" alt="">
+                        <img src="../assets/logo.png" alt="">
                     </div>
                 </el-col>
                 <el-col :span="19" class="middle">
                     <h2>电商后台管理系统</h2>
                 </el-col>
                 <el-col :span="1">
-                    <a href="#"  class="loginout">退出</a>
+                    <a href="#" class="loginout">退出</a>
                 </el-col>
             </el-row>
         </el-header>
         <el-container>
-            <el-aside class="aside" width="200px">Aside</el-aside>
+            <el-aside class="aside" width="200px">
+                <!-- 导航 -->
+                <el-aside class="aside" width="200px">
+                    <!-- default-active:默认展开项
+                    :unique-opened: 只允许同时展开一项
+                    :router 开启路由模式
+                    开启后, 设置el-menu-item的index值 为路由标识
+                    -->
+                    <el-menu default-active="2-1" class="menu" :unique-opened="true" :router="true">
+                        <!-- 用户管理-->
+                        <el-submenu index="1">
+                            <template slot="title">
+                                <i class="el-icon-location"></i>
+                                <span>用户管理</span>
+                            </template>
+                            <el-menu-item index="/user">
+                                <i class="el-icon-menu"></i>
+                                用户列表
+                            </el-menu-item>
+                        </el-submenu>
+                        <!-- 权限管理-->
+                        <el-submenu index="2">
+                            <template slot="title">
+                                <i class="el-icon-location"></i>
+                                <span>权限管理</span>
+                            </template>
+                            <el-menu-item index="/roles">
+                                <i class="el-icon-menu"></i>
+                                角色列表
+                            </el-menu-item>
+                            <el-menu-item index="/rights">
+                                <i class="el-icon-menu"></i>
+                                权限列表
+                            </el-menu-item>
+                        </el-submenu>
+                        <!-- 商品管理-->
+                        <el-submenu index="3">
+                            <template slot="title">
+                                <i class="el-icon-location"></i>
+                                <span>商品管理</span>
+                            </template>
+                            <el-menu-item index="3-1">
+                                <i class="el-icon-menu"></i>
+                                商品列表
+                            </el-menu-item>
+                            <el-menu-item index="3-1">
+                                <i class="el-icon-menu"></i>
+                                分类参数
+                            </el-menu-item>
+                            <el-menu-item index="3-1">
+                                <i class="el-icon-menu"></i>
+                                商品分页
+                            </el-menu-item>
+                        </el-submenu>
+                        <!--订单管理-->
+                        <el-submenu index="4">
+                            <template slot="title">
+                                <i class="el-icon-location"></i>
+                                <span>订单管理</span>
+                            </template>
+                            <el-menu-item index="4-1">
+                                <i class="el-icon-menu"></i>
+                                订单列表
+                            </el-menu-item>
+                        </el-submenu>
+                        <!--数据统计-->
+                        <el-submenu index="5">
+                            <template slot="title">
+                                <i class="el-icon-location"></i>
+                                <span>数据统计</span>
+                            </template>
+                            <el-menu-item index="5-1">
+                                <i class="el-icon-menu"></i>
+                                数据报表
+                            </el-menu-item>
+                        </el-submenu>
+
+                    </el-menu>
+                </el-aside>
+            </el-aside>
             <el-main class="main">Main</el-main>
         </el-container>
     </el-container>
 </template>
 
 <script>
-export default {};
+export default {}
 </script>
 
 <style>
@@ -32,17 +112,20 @@ export default {};
   background-color: #b3c0d1;
 }
 .middle {
-    text-align: center;
-    line-height: 60px;
+  text-align: center;
+  line-height: 60px;
 }
-.aside {
+/* .aside {
   background: red;
+} */
+.aside .menu {
+  height: 100%;
 }
 .main {
   background: gray;
 }
 .loginout {
-    line-height: 60px;
-    text-decoration: none;
+  line-height: 60px;
+  text-decoration: none;
 }
 </style>
