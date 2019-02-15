b7fefb94eacde35123e5029280cd7a94bbe343ac
cwc <2534385076@qq.com>
2019-02-15 17:10:21 +0800

    首页-头部布局完成

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,6 +1,20 @@
 <template>
     <el-container class="container">
-        <el-header>Header</el-header>
+        <el-header class="header">
+            <el-row>
+                <el-col :span="4">
+                    <div class="grid-content bg-purple">
+                    <img src="../assets/logo.png" alt="">
+                    </div>
+                </el-col>
+                <el-col :span="19" class="middle">
+                    <h2>电商后台管理系统</h2>
+                </el-col>
+                <el-col :span="1">
+                    <a href="#"  class="loginout">退出</a>
+                </el-col>
+            </el-row>
+        </el-header>
         <el-container>
             <el-aside class="aside" width="200px">Aside</el-aside>
             <el-main class="main">Main</el-main>
@@ -9,18 +23,26 @@
 </template>
 
 <script>
-export default {}
+export default {};
 </script>
 
 <style>
 .container {
-    height: 100%;
-    background-color: yellow;
+  height: 100%;
+  background-color: #b3c0d1;
 }
-.aside{
-    background: red;
+.middle {
+    text-align: center;
+    line-height: 60px;
 }
-.main{
-    background: gray;
+.aside {
+  background: red;
+}
+.main {
+  background: gray;
+}
+.loginout {
+    line-height: 60px;
+    text-decoration: none;
 }
 </style>
