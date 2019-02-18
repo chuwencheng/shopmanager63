719f96eef7b8288871a7061e53fd66aee41d3794
cwc <2534385076@qq.com>
2019-02-18 15:47:10 +0800

    角色组件布局完成

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,7 @@ import Login from '@/components/login.vue'
 import Home from '@/components/home.vue'
 import Users from '@/components/users.vue'
 import Rights from '@/components/rights.vue'
+import Roles from '@/components/roles.vue'
 
 Vue.use(Router)
 
@@ -27,6 +28,9 @@ export default new Router({
       name: 'rights',
       path: '/rights',
       component: Rights
-    }]
+    },{
+      name: 'roles',
+      path: '/roles',
+      component: Roles}]
   }]
 })
