3b58b4fddef64d7dd0acf146764a1975cfc9ac6f
cwc <2534385076@qq.com>
2019-02-18 17:24:39 +0800

    取消权限

--- src/components/roles.vue
+++ src/components/roles.vue
@@ -10,21 +10,24 @@
                 <template slot-scope="scope">
                     <el-row class="level1" v-for="(item1,i) in scope.row.children" :key="item1.id">
                         <el-col :span="4">
-                            <el-tag closable type="danger">{{item1.authName}}</el-tag>
+                            <el-tag @close="deleRights(scope.row,item1)" closable type="danger">{{item1.authName}}</el-tag>
                             <i class="el-icon-arrow-right"></i>
                         </el-col>
                         <el-col :span="20">
                             <el-row class="level2" v-for="(item2,i) in item1.children" :key="item2.id">
                                 <el-col :span="4">
-                                    <el-tag closable type="info">{{item2.authName}}</el-tag>
+                                    <el-tag @close="deleRights(scope.row,item2)" closable type="info">{{item2.authName}}</el-tag>
                                     <i class="el-icon-arrow-right"></i>
                                 </el-col>
                                 <el-col :span="20">
-                                    <el-tag closable 
-                                    class="level3" v-for="(item3,i) in item2.children" :key="item3.id" 
-                                    type="warning">{{item3.authName}}</el-tag>
+                                    <el-tag @close="deleRights(scope.row,item3)" closable class="level3" v-for="(item3,i) in item2.children" :key="item3.id" type="warning">{{item3.authName}}</el-tag>
                                 </el-col>
                             </el-row>
+                        </el-col>
+                    </el-row>
+                    <el-row v-if="scope.row.children.length===0">
+                        <el-col>
+                            <span>未分配权限</span>
                         </el-col>
                     </el-row>
                 </template>
@@ -49,27 +52,42 @@
 
 <script>
 export default {
-  data () {
+  data() {
     return {
       roles: []
-    }
+    };
   },
-  created () {
-    this.getRoles()
+  created() {
+    this.getRoles();
   },
   methods: {
+    // 点击×，删除权限
+    async deleRights(role,rights) {
+        // console.log(role,rights)
+        const res = await this.$http.delete(`roles/${role.id}/rights/${rights.id}`)
+        const {meta:{msg,status},data} = res.data
+        // console.log(data)// data是剩余权限
+        if (status === 200) {
+            // 提示信息
+            this.$message.success(msg)
+            // 更新
+            // this.getRoles()
+            // 优化 - 只更新当前角色权限
+            role.children = data
+        }
+    },
     // 点击√，显示权限对话框
-    showDiaSetRights () {
+    showDiaSetRights() {
 
     },
-    async getRoles () {
-      const res = await this.$http.get(`roles`)
+    async getRoles() {
+      const res = await this.$http.get(`roles`);
       //   console.log(res)
-      this.roles = res.data.data
-      console.log(this.roles)
+      this.roles = res.data.data;
+    //   console.log(this.roles);
     }
   }
-}
+};
 </script>
 
 <style>
@@ -79,8 +97,9 @@ export default {
 .btn {
   margin-top: 20px;
 }
-.level1,.level2 {
-    margin-bottom: 10px;
+.level1,
+.level2 {
+  margin-bottom: 10px;
 }
 </style>
 // 展开行拆分方式
